d421791910e4e3f28b5a5a52b2d59db030e0167d
Preston <PrestonT500WasTaken@proton.me>
2025-02-26 14:44:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Watching The Server", "Listening To Commands", "Playing Nova Network", "Helping Members"];
+const statusTypes = [ 'online"];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
